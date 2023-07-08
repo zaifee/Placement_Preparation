e3a75b56a53a254828eb7fca6ee5d9be98fceb7a
Mohd Nawaz <94606718+zaifee@users.noreply.github.com>
2023-07-07 22:17:29 -0700

    Create Pattern2.c

---
+++ Pattern2.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+void main()
+{
+    int i, n;
+    printf("Enter any Number of Your choice : ");
+    scanf("%d", &n);
+    for(i = 1; i<=n; i++){
+        int j;
+        for(j = 1; j<=n; j++){
+            if(j <= 6-i){
+                printf(" *");
+            }else{
+                printf(" ");
+            }
+        }
+        printf("\n");
+    }
+    
+    
+}
