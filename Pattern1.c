f16d12cc9b3e8a07b8e0693a4106cd015d692440
Mohd Nawaz <94606718+zaifee@users.noreply.github.com>
2023-07-07 22:11:32 -0700

    Create Pattern1.c

---
+++ Pattern1.c
@@ -0,0 +1,31 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+  GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+  C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, COBOL, HTML, CSS, JS
+  Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+   /*
+   * * * * *
+   * * * * *
+   * * * * *
+   * * * * *
+  */
+     
+    int i;
+    for(i =1; i<=4; i++){
+        
+    int j;
+    for(j = 1; j<=5; j++){
+        printf(" * ");
+    }
+    printf("\n");
+    }
+
+    return 0;
+}
