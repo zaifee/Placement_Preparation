740e6f497d63aae4b06113f1416c04b9b0b6ef8d
Mohd Nawaz <94606718+zaifee@users.noreply.github.com>
2023-07-07 22:54:22 -0700

    Create Pattern4.c

---
+++ Pattern4.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/*
+        *
+      * *
+    * * *
+  * * * *
+* * * * *
+*/
+
+
+int main()
+{
+    int i, j;
+    for(i = 1; i<=5; i++){
+        for(int j = 1; j<=5; j++){
+            if(j >= 6-i){
+                printf("*");
+            }else{
+                printf(" ");
+            }
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
