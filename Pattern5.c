f8452f9450d0f88df87959bd26f939a4e3396dcb
Mohd Nawaz <94606718+zaifee@users.noreply.github.com>
2023-07-07 23:52:53 -0700

    Create Pattern5.c

---
+++ Pattern5.c
@@ -0,0 +1,24 @@
+
+#include <stdio.h>
+/*
+      *
+    * * *
+  * * * * *
+ * * * * * * *
+*/
+int main()
+{
+     
+     for(int i = 1; i<=4; i++){
+         for(int j = 1; j<=7; j++){
+             if(j >= 5-i && j <= 3+i){
+                 printf("*");
+             }else{
+                 printf(" ");
+             }
+         }
+         printf("\n");
+     }
+   
+    return 0;
+}
