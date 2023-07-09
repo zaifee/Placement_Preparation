b2fb6b304ab2c5831fa6b60c83565f7850c102d1
Mohd Nawaz <94606718+zaifee@users.noreply.github.com>
2023-07-08 23:00:55 -0700

    Create Pattern7.c

---
+++ Pattern7.c
@@ -0,0 +1,30 @@
+
+
+#include <stdio.h>
+/*
+* * * * * * *
+* * *   * * *
+* *       * * 
+*           *
+
+*/
+int main()
+{
+    int i, j, k;
+    for(i = 1; i<=4; i++){
+        
+        for(j = 1;  j<=7; j++){
+            
+            if(j < 6-i || j >= 3+i){
+                printf("*");
+                
+            }else{
+                printf(" ");
+               
+            }
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
