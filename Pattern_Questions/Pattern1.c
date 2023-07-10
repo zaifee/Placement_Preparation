803f41dca787aa1e04a3456cb41b585756ad3ac3
Mohd Nawaz <94606718+zaifee@users.noreply.github.com>
2023-07-10 03:26:20 -0700

    Update Pattern1.c

--- Pattern_Questions/Pattern1.c
+++ Pattern_Questions/Pattern1.c
@@ -1,1 +1,23 @@
+#include <stdio.h>
 
+int main()
+{
+   /*
+   * * * * *
+   * * * * *
+   * * * * *
+   * * * * *
+  */
+     
+    int i;
+    for(i =1; i<=4; i++){
+        
+    int j;
+    for(j = 1; j<=5; j++){
+        printf(" * ");
+    }
+    printf("\n");
+    }
+
+    return 0;
+}
