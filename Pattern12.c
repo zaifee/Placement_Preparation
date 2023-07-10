405ebc7cda9444cdaf894b29e71c43cfebffd5b8
Mohd Nawaz <94606718+zaifee@users.noreply.github.com>
2023-07-10 02:57:05 -0700

    Create Pattern12.c

---
+++ Pattern12.c
@@ -0,0 +1,21 @@
+
+#include <stdio.h>
+/*
+1 2 3 4
+1 2 3 4
+1 2 3 4
+1 2 3 4
+*/
+
+int main()
+{
+//   int count = 1;
+   for(int i=1; i<=4; i++){
+       for(int j=1; j<=4; j++){
+           printf("%d", j);
+        //   count++;
+       }
+       printf("\n");
+   }
+   
+   
