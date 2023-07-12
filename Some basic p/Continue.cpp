b8100443966abec27c1fc2e1cdfaeab7628fb0d2
Mohd Nawaz <94606718+zaifee@users.noreply.github.com>
2023-07-12 04:16:37 -0700

    Create Continue.cpp

---
+++ Some basic p/Continue.cpp
@@ -0,0 +1,22 @@
+
+#include <iostream>
+
+using namespace std;
+//continue statement
+int main()
+{
+    // for(int i=1; i<=10; i++){
+    //     if(i % 3 == 0)
+    //       continue;
+    //      cout<<i<<" ";
+    // }
+    
+    for(int i=1; i<=5; i++){
+        cout<<"Hey"<<" ";
+        cout<<"jelly"<<" ";
+        continue;
+        cout<<"Reply to karde "<<endl;
+    }
+   
+    return 0;
+}
