12ce4e99fe40552c8d870aef9a9a72c259ac37a7
Mohd Nawaz <94606718+zaifee@users.noreply.github.com>
2023-07-17 02:13:09 -0700

    Update deque.cpp

--- C++ STL/deque.cpp
+++ C++ STL/deque.cpp
@@ -1,61 +1,67 @@
 
 #include <iostream>
-#include<vector>
+#include<deque>
+
 
 using namespace std;
 
 int main()
 {
-    // Declaring the vector if we already know the Size
-    vector<int> v1(3, 9);
-    for(int i: v1){
-        cout<<i<<" ";
-    }cout<<endl;
-    
-    // copying vector into another vector
-    vector<int> copying(v1);
-    for(int i: copying){
-        cout<<i<<" ";
-    }cout<<endl;
-    
-    
-    
-    
-    vector<int> v;
-    cout<<"Capacity means memory allocate to the vector "<<v.capacity()<<endl;
-    
-    v.push_back(1);
-    cout<<"Capacity means memory allocate to the vector "<<v.capacity()<<endl;
-    
-    v.push_back(2);
-    cout<<"Capacity means memory allocate to the vector "<<v.capacity()<<endl;
-    
-    v.push_back(3);
-    cout<<"Capacity means memory allocate to the vector "<<v.capacity()<<endl;
-    
-    cout<<"Size of the vector "<<v.size()<<endl;
-    cout<<"Element at Index "<<v.at(0);
-    
-    cout<<"Element at front "<<v.front()<<endl;
-    cout<<"Element at back "<<v.back()<<endl;
-    
+//   Deque is the type of element in which we can do insertion and deletion from both ends
+   deque<int> d;
+   d.push_back(1);
+   d.push_back(2);
+   d.push_front(0);
+   for(int i: d){
+       cout<<i<<" ";
+   }cout<<endl;
+   
+//   for pop the element 
     cout<<"Before pop "<<endl;
-    for(int i: v){
+    for(int i: d){
         cout<<i<<" ";
     }cout<<endl;
-    v.pop_back();
+    d.pop_back();
     cout<<"After pop "<<endl;
-    for(int i: v){
+    for(int i: d){
         cout<<i<<" ";
     }cout<<endl;
     
-    // clear function clear the vector element not the memory assigned to the vector
-    cout<<"Before Clear the Size of the vector is : "<<v.size()<<endl;
-    v.clear();
-    cout<<"After Clear the Size of the vector is : "<<v.size()<<endl;
+// can we pop the front element let's try  !!! Not possible
+ cout<<"front element "<<d.front()<<endl;
+ cout<<"Back element "<<d.back()<<endl;
+  
+  cout<<"Checking queue is empty ?:"<<d.empty()<<endl;
+  cout<<"Element at Index :"<<d.at(1)<<endl;
+// erase function takes argument to erase the size of the queue becomes 0 after erase the max size remains same.
+ cout<<"Before erase: "<<d.size()<<endl;
+  d.erase(d.begin(), d.begin()+1);
+  cout<<"After erase: "<<d.size()<<endl;
+  for(int i: d){
+      cout<<i<<" ";
+  }cout<<endl;
+  
+  
+  d.push_front(0);
+  d.push_back(2);
+  for(int i: d){
+      cout<<i<<" ";
+  }cout<<endl;
+  
+  d.pop_back();
+  for(int i: d){
+      cout<<i<<" ";
+  }cout<<endl;
+  
+  
     
-    
-    
-    
+   
+   
+   
     return 0;
 }
+
+    
+
+    
+  
