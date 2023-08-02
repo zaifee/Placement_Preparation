bc089db3b6115735afe1acf9c9094b5a144e085e
Mohd Nawaz <94606718+zaifee@users.noreply.github.com>
2023-08-02 12:16:13 -0700

    Create One's all element.cpp

---
+++ Array/One's all element.cpp
@@ -0,0 +1,36 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+#include<cstring>
+#include<array>
+
+using namespace std;
+// 1 1 1 1 
+int main()
+{
+   int arr[100] = {1, 3, 5, 7, 6, 4, 6};
+//   int a[10];
+//     memset(arr, 1, sizeof(arr));
+//     for(int i=0; i<7; i++)
+//         cout << arr[i] << " ";
+//         cout << endl;
+        
+    
+// //   taking an array
+// for(int i=0; i<5; i++){
+//     arr[i] = 1;
+// }
+
+// // Printing the 1 instead of other element
+// for(int i=0; i<5; i++){
+//     cout << arr[i] << " ";
+// }
+
+    return 0;
+}
