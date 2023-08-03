09ead44201cb20d01cad1625add45b58af89f626
Mohd Nawaz <94606718+zaifee@users.noreply.github.com>
2023-08-03 11:52:19 -0700

    Create Extreme_print.cpp

---
+++ Array/Extreme_print.cpp
@@ -0,0 +1,31 @@
+// Extreme Print in array;
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int arr[] = {10, 20, 30, 40, 50, 60 ,70, 80, 90};
+    int size = 9;
+    int start = 0;
+    int end = size-1;
+    while(true){
+        if(start > end){
+           
+            break;
+        }
+        if(start == end){
+          cout << arr[start] << " ";
+        }else{
+         cout << arr[start] << " ";
+        cout << arr[end] << " ";
+            
+        }
+        
+        start++;
+        end--;
+    }
+   
+
+    return 0;
+}
