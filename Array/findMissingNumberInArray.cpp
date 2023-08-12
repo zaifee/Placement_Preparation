520d5819c317015779ef60c037b938c00b8116cb
Mohd Nawaz <94606718+zaifee@users.noreply.github.com>
2023-08-11 23:54:03 -0700

    Update findMissingNumberInArray.cpp

--- Array/findMissingNumberInArray.cpp
+++ Array/findMissingNumberInArray.cpp
@@ -29,3 +29,46 @@ int main() {
 
   return 0;
 }
+
+
+
+// Method 2 Sorting and Swapping Method
+
+#include <iostream>
+using namespace std;
+void findMissingElement(int arr[], int n){
+  int i=0;
+  while(i < n){
+    // Array index start from 0 so we are doing -1
+    int index =  arr[i]-1;
+    //  current element is not equal to index than we are swapping it
+    if(arr[i] != index){
+      swap(arr[index], arr[i]);
+    }
+    ++i;
+  }
+//   Printing the Missing Element using i+1; we assuming index start from 1 but actually start from 0
+  for(int i=0; i<n; i++){
+    if(arr[i] != i+1){
+        cout << i+1 << " ";
+    }
+  }
+}
+
+
+
+int main() {
+ 
+  int arr[] = {1, 5, 3, 3, 5};
+  int n = sizeof(arr)/sizeof(int);
+
+  findMissingElement(arr, n);
+
+
+
+  return 0;
+}
+
+
+
+
