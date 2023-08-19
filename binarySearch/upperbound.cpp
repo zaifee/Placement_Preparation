ad48998f89f62590245e02498c16ec8dc78c2296
Mohd Nawaz <94606718+zaifee@users.noreply.github.com>
2023-08-19 07:06:41 -0700

    Create upperbound.cpp

---
+++ binarySearch/upperbound.cpp
@@ -0,0 +1,18 @@
+int upperBound(vector<int> &arr, int x, int n){
+	int s = 0;
+	int e = n-1;
+	int mid = s + (e-s)/2;
+	int ans = n;
+		while(s <= e){
+		if(arr[mid] > x){
+			ans = mid;
+			e = mid - 1;
+			
+		}else{
+			
+			s = mid + 1;
+		}
+		mid = s + (e-s)/2;
+	}
+	return ans;	
+}
